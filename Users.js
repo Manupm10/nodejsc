40a6dd1dcd886f25fd2bb880dc7e0cb99d8b7672
Manupm10 <manupm3465@gmail.com>
2022-10-22 13:15:23 +0530

    first commit

---
+++ Users.js
@@ -0,0 +1,24 @@
+const users = [
+    {
+        id:1,
+        name:'John',
+        email:'john@email.com'
+    },
+    {
+        id:2,
+        name:'Manu',
+        email:'manu@gmail.com'
+    },
+    {
+        id:3,
+        name:'Smith',
+        email:'smith@gmail.com'
+    },
+    {
+        id:4,
+        name:'Ram',
+        email:'ram@gmail.com'
+    },
+]
+
+module.exports = users;
